2d4be08fcb99f17d160c22a514e859498d6e5f9a
jankenstein <eric.r.jankowski@gmail.com>
2013-04-22 22:45:54 -0500

    counting us states

---
+++ src/main/java/com/erj/practice/hadoop/patent/data/CountUsStates.java
@@ -0,0 +1,82 @@
+package com.erj.practice.hadoop.patent.data;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.KeyValueTextInputFormat;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+
+@SuppressWarnings("deprecation")
+public class CountUsStates extends Configured implements Tool{
+	private static IntWritable one = new IntWritable(1);
+	
+	public static class ImTheMap extends MapReduceBase implements Mapper<Text, Text, Text, IntWritable> {
+		public void map(Text key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+			//Parse the line and make a tick mark for each US state 
+			String line = key.toString();
+			line = line.replaceAll("\"", "");
+			String [] components = line.split(",");
+			try{
+				Integer.parseInt(components[0]);
+				if(components[4].equals("US") && components[5].length()>0){
+					output.collect(new Text(components[5]), one);
+				}
+			}catch(NumberFormatException e){}
+		}
+	}
+			
+	public static class Reducto extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+			//Add up the tick marks
+			int count = 0;
+			while (values.hasNext()){
+				count += values.next().get();
+			}
+			output.collect(key, new IntWritable(count));
+		}
+	}
+	
+	public int run (String [] args) throws Exception {
+		Configuration conf = getConf();
+		JobConf job = new JobConf(conf, CountUsStates.class);
+		Path in = new Path(args[0]);
+		Path out = new Path(args[1]);
+		FileInputFormat.setInputPaths(job, in);
+		FileOutputFormat.setOutputPath(job, out);
+		
+		job.setJobName("StateCount");
+		job.setMapperClass(ImTheMap.class);
+		job.setReducerClass(Reducto.class);
+		job.setInputFormat(KeyValueTextInputFormat.class);
+		job.setOutputFormat(TextOutputFormat.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		JobClient.runJob(job);
+		
+		return 0;
+	}
+	
+	public static void main (String [] args) throws Exception {
+		int res = ToolRunner.run(new Configuration(),  new CountUsStates(), args);
+		System.exit(res);
+	}
+}
+
---
+++ src/test/java/com/erj/practice/hadoop/patent/data/CountUsStatesTest.java
@@ -0,0 +1,125 @@
+package com.erj.practice.hadoop.patent.data;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CountUsStatesTest {
+	
+	CountUsStates.ImTheMap mapper;
+	CountUsStates.Reducto reducer;
+	Text key;
+	Text value;
+	OutputCollector<Text, IntWritable> output; 
+	Reporter reporter;
+	Iterator<IntWritable> values;
+	
+	final List<IntWritable> prep = new ArrayList<IntWritable>();
+	
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp(){
+		mapper = new CountUsStates.ImTheMap();
+		reducer = new CountUsStates.Reducto();
+		key = new Text();
+		value = new Text();
+		
+		output = mock(OutputCollector.class);
+		
+		for(int i=0;i<prep.size();i++){
+			prep.remove(0);
+		}
+		values = new Iterator<IntWritable>(){
+			List<IntWritable> list = prep;
+						
+			public boolean hasNext() {
+				return list.size() > 0;
+			}
+
+			public IntWritable next() {
+				return list.remove(0);
+			}
+
+			public void remove() {}
+			
+		};
+	}
+
+	
+	@Test
+	public void mapCollectsOneTickMarkPerRow() throws IOException {
+		key.set("3070801,1963,1096,,\"US\",\"TX\",,1,,269,6,69,,1,,0,,,,,,,");
+				
+		mapper.map(key, value, output, reporter);
+		
+		verify(output).collect(eq(new Text("TX")), eq(new IntWritable(1)));
+	}
+	
+	@Test
+	public void mapShouldNotCollectIfKeyDoesNotContainAPatentNumber() throws IOException {
+		key.set("BadDates,1963,1096,,\"US\",\"TX\",,1,,269,6,69,,1,,0,,,,,,,");
+				
+		mapper.map(key, value, output, reporter);
+		
+		verify(output, never()).collect(any(Text.class), eq(new IntWritable(1)));
+	}
+	
+	@Test
+	public void mapShouldNotCollectIfCountryIsNotUnitedStates() throws IOException {
+		key.set("3070801,1963,1096,,\"BE\",\"TX\",,1,,269,6,69,,1,,0,,,,,,,");
+				
+		mapper.map(key, value, output, reporter);
+		
+		verify(output, never()).collect(any(Text.class), eq(new IntWritable(1)));
+	}
+	
+	@Test
+	public void mapShouldNotCollectIfNoUsStateIsIndicated() throws IOException {
+		key.set("3070801,1963,1096,,\"US\",\"\",,1,,269,6,69,,1,,0,,,,,,,");
+				
+		mapper.map(key, value, output, reporter);
+		
+		verify(output, never()).collect(any(Text.class), eq(new IntWritable(1)));
+	}
+	
+	@Test
+	public void reduceCountsAndCollectsTheNumberOfCitations() throws IOException{
+		prep.add(new IntWritable(1));
+		prep.add(new IntWritable(1));
+		
+		reducer.reduce(key, values, output, reporter);
+		
+		verify(output).collect(eq(key), eq(new IntWritable(2)));
+	}
+	
+	@Test
+	public void reduceCountsAndCollectsTheNumberOfCitationsThatCantBeHardCoded() throws IOException{
+		int firstNumber = (int)(Math.random() * 100);
+		int secondNumber = (int)(Math.random() * 100);
+		int sum = firstNumber + secondNumber;
+		
+		prep.add(new IntWritable(firstNumber));
+		prep.add(new IntWritable(secondNumber));
+		
+		reducer.reduce(key, values, output, reporter);
+		
+		verify(output).collect(eq(key), eq(new IntWritable(sum)));
+		
+	}
+
+}
